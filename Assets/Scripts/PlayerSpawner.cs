fbf18c845764c24076fada6b1cdb84452a2695c6
Alexander Shust <ashust@mail.ru>
2023-07-13 00:23:08 +0300

    Spawn player

--- Assets/Scripts/PlayerSpawner.cs
+++ Assets/Scripts/PlayerSpawner.cs
@@ -4,19 +4,14 @@ using UnityEngine;
 public class PlayerSpawner : SimulationBehaviour, ISpawned
 {
     [SerializeField] private GameObject _playerPrefab;
-    
-    private void OnEnable()
-    {
-        Debug.LogWarning("Started!");
-    }
 
     public void Spawned()
     {
-        Debug.LogWarning("Spawned!");
+        SpawnPlayer(Runner.LocalPlayer);
     }
 
     private void SpawnPlayer(PlayerRef player)
     {
-        
+        Runner.Spawn(_playerPrefab, new Vector3(player.PlayerId, 0, 0), Quaternion.identity, player);
     }
 }
