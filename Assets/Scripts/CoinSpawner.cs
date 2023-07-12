4466422822d21c8358b9d278a391a4074fc59a48
Alexander Shust <ashust@mail.ru>
2023-07-13 00:53:31 +0300

    Coin spawner

---
+++ Assets/Scripts/CoinSpawner.cs
@@ -0,0 +1,16 @@
+﻿using Fusion;
+using UnityEngine;
+
+public class CoinSpawner : SimulationBehaviour, ISpawned
+{
+    [SerializeField] private GameObject _coinPrefab;
+    
+    public void Spawned()
+    {
+        if (!Object.HasStateAuthority)
+        {
+            return;
+        }
+        Debug.LogWarning("Spawn coin");
+    }
+}
